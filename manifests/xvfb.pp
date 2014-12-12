2bbbfa34069ebb6af55d9a7567e2deae072ef6b1
Josh Beard <beard@puppetlabs.com>
2014-12-12 12:32:18 -0500

    Add option to run xvfb as custom user
    This commit allows running xvfb as a custom user (e.g. non-root).
    
    - A 'runuser' parameter was added to the display::xvfb class to specify
      the user, which defaults to 'root'.
    - An 'fbdir' parameter was added to the display::xvfb class to allow a
      custom Xvfb 'fbdir'.  Previously, this was hard-set to '/var/run',
      which could result in issues as non-root users.  It also appears this
      is most commonly set to a temp directory (e.g. /tmp or /usr/tmp).
      Defaults to '/tmp'.
    - The RedHat init script template was updated to utilize the new
      paramters. Specifically, this passes the 'runuser' to the 'daemon'
      init function and updates the command-line option for 'fbdir' to use
      the custom value.

--- manifests/xvfb.pp
+++ manifests/xvfb.pp
@@ -3,6 +3,28 @@
 # Installs and configures xvfb.
 # Inspired by https://github.com/cwarden/puppet-module-xvfb.
 #
+# === Parameters
+#
+# [*display*]
+#   X display to use. Default is 0.
+# [*width*]
+#    Screen width to use. Default is 1280.
+# [*height*]
+#    Screen height to use. Default is 800.
+# [*color*]
+#    Screen color depth to use. Default is "24+32" (32 bit).
+# [*runuser*]
+#    User to run xvfb as. Default is 'root'.
+# [*fbdir*]
+#    Directory in which the memory mapped files containing the framebuffer
+#    memory should be created. Defaults to '/tmp'
+# [*package*]
+#    Package name for installing xvfb. Defaults to 'xorg-x11-server-Xvfb' on
+#    RedHat systems and 'xvfb' on Debian systems.
+# [*service*]
+#    Name of the xvfb service. This class will create an initscript with this
+#    name and manage a service with this name.  Defaults to 'xvfb'
+#
 # === Authors
 #
 # Alex Rodionov <p0deje@gmail.com>
@@ -16,6 +38,8 @@ class display::xvfb (
   $width   = 1280,
   $height  = 768,
   $color   = '24+32',
+  $runuser = 'root',
+  $fbdir   = '/tmp',
   $package = $display::params::xvfb_package_name,
   $service = $display::params::xvfb_service_name,
 ) inherits display::params {
@@ -25,7 +49,7 @@ class display::xvfb (
     alias  => 'xvfb',
   }
 
-  file { '/etc/init.d/xvfb':
+  file { "/etc/init.d/${service}":
     ensure  => 'file',
     content => template($display::params::xvfb_erb),
     mode    => '0755',
